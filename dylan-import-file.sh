e90eae7c4577acf2ea8cb509797c606a25ecac71
Dylan Cascio <dcascio73@gmail.com>
2017-04-24 22:38:24 -0700

    Created dylan-import script
    Currently in planning stages, no code as yet

---
+++ dylan-import-file.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# create new file to store todo
+# dir name == file name
+
+# file cleaning
+# should this clean the text?
+# i.e. convert all tabs to spaces?
+
+# read each line sequentially
+# count the number of spaces/tabs
+
+# assign the number of spaces to a directory level
+
+# dave used a create leaf function,
+# this would serve to identify the
+# end of a file system
+
+# d also mentioned converting each line 
+# as a file unless a new file is discovered
+# below it. 
+
+# read line
+# count spaces
+# assign level in tree to line
+
+# create new file
+
+# if file has leaf below
+# turn it into a node
+    # remove file, create dir with same file name
+    # move into that dir
+
+
