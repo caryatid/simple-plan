ad88593a43966d74452f10bffb4852a7cdabc2ff
Dave Setchell <setchell.dave@gmail.com>
2017-05-05 13:55:15 -0700

    kinda working

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ All of this should be done in POSIX shell.
 TODO file should be parsed so that top level directories are created for each line in the todo without indentation
 deeper indentation indicates a child of the highter indentation then that entry becomes a sub file/directory to the one created for the line above.
 
-1) take the example format, TODO.txt, and make it into a filesystem structure. 
-2) generate a directory of symlinks to all of the files within that filesystem strucuture.
+1) take the example format, TODO.txt, and make it into a filesystem structure.
+2) generate a directory of symlinks to all of the files within that 
+   filesystem strucuture.
 3) implement a way to present those symlinks in an orderable manner
-4) present possible improvements. For example, does this need its own search or can it simply use grep
-
+4) Is there an issue with how you handle changing existing files into a
+   directory and a file?
+5) can this take a directory and dump to a file?
+6) Does this need its own search or can it simply use grep
+7) perhaps implement some commands to add or remove entries.
+   May just switch to plan proper
--- import-file.sh
+++ import-file.sh
@@ -4,14 +4,33 @@ trap 'rm -Rf $TMP' EXIT
 
 # convert to directory nodes; file leaves.
 
-test -f "$1" || exit 1
+
+seed=$(dd if=/dev/urandom bs=255 count=1 2>/dev/null | tr \\0 \ )
+count=0
+_gen_hash () {
+    echo -n $count$seed | sha1sum | cut -d' ' -f1 | tr -d '\n'
+    count=$(( $count + 1 ))
+}
 
 _make_leaf () {
     test -z "$1" && exit 1
-    test ! -e "$1" || touch "$1"
+    test -e "$1" || touch "$1"
+}
+
+_no_empty () {
+    sed '/^[[:space:]]*$/d'
+}
+
+_pop () {
+    val=$(<$TMP/stack _no_empty | tail -n1)
+    test -z "$val" && return 1
+    <$TMP/stack _no_empty | head -n-1  >$TMP/_stack
+    mv $TMP/_stack $TMP/stack
 }
 
 _parse_note_file () {
+    bdir="__${1}__"
+    mkdir -p "$bdir"; cd "$bdir"
     truncate -s 0 $TMP/stack; pwd >$TMP/stack
     local pname=''
     local pdepth=0
@@ -19,29 +38,40 @@ _parse_note_file () {
     do
         local depth=$(echo $REPLY | cut -d' ' -f1)
         local name=$(echo $REPLY | cut -d' ' -f2-)
-        case $(( $depth - $pdepth )) in
+        local dif=$(( $depth - $pdepth ))
+        echo $depth $dif $name
+        case $dif in
         0)
             _make_leaf "$name"
             pname="$name"
             ;;
         1)
-            test -f "$pname" && mv "$pname" "${pname}_"
-            mkdir "$pname"; cd "$pname"
+            if test -f "$pname"
+            then 
+                local h=$(_gen_hash | cut -c-7)
+                mv "$pname" "${pname}_${h}"
+                mkdir -p "$pname"; mv "${pname}_${h}" "${pname}"
+            fi
+            cd "$pname"
             echo "$pname" >>$TMP/stack
             _make_leaf "$name"
+            pname="$name"
+            pdepth=$depth
             ;;
         *)        
-            echo breakfast is more important than implementation
+            for _ in $(seq $(( $dif * -1 ))); do _pop; cd ..; done
+            _make_leaf "$name"
+            pdepth=$depth
+            pname="$name"
             ;;
         esac
     done
 }
-                
-            
-                
 
-<"$1" sed '/^[[:space:]]*$/d' \
+test -f "$1" || exit 1
+FNAME="$1"
+<"$FNAME" _no_empty \
     | awk '{ match($0, /^[[:space:]]*/); print RLENGTH/4, $0 }' \
-    | _parse_note_file
+    | _parse_note_file "$FNAME"
 
     
